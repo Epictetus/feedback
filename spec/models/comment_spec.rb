af713eda24fd9f22e58ae859f233ae3b37507e69
Taurus Colvin <taurus.colvin@gmail.com>
2012-04-25 15:33:37 -0400

    get acts_as_commentable specs to pass; use new ruby hash style

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe Comment do
   before(:each) do
-    @user = create(:user)
-    @blog_post = create(:blog_post)
-    @comment = Comment.new(:commentable => @blog_post, :user => @user)
+    @user = create(:user, name: "Bill Bixby")
+    @blog_post = create(:blog_post, title: "A great thought")
+    @comment = Comment.new(commentable: @blog_post, user: @user)
   end
   
   it "accepts a commentable" do
