40c8cbda5561b822d3283a5cc97df23d434746a5
Taurus Colvin <taurus.colvin@gmail.com>
2012-04-26 19:18:12 -0400

    add index to improve performance

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120425185333) do
+ActiveRecord::Schema.define(:version => 20120426215701) do
 
   create_table "blog_posts", :force => true do |t|
     t.integer  "user_id"
@@ -30,6 +30,8 @@ ActiveRecord::Schema.define(:version => 20120425185333) do
     t.datetime "created_at",       :null => false
     t.datetime "updated_at",       :null => false
   end
+
+  add_index "comments", ["commentable_id", "commentable_type"], :name => "index_comments_on_commentable_id_and_commentable_type"
 
   create_table "users", :force => true do |t|
     t.string   "name"
