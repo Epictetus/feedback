b63097bb64275adfb5f233cb86015dae99646409
Taurus Colvin <taurus.colvin@gmail.com>
2012-05-06 18:39:14 -0400

    no longer need auth scaffolding here

--- spec/dummy/app/controllers/application_controller.rb
+++ spec/dummy/app/controllers/application_controller.rb
@@ -1,17 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  
-  private
-  
-  def authenticate_user!
-    session[:current_user_id] = stub_user.id
-  end
-  
-  def current_user
-    User.find(session[:current_user_id])
-  end
-
-  def stub_user
-    User.first || User.create(:name => "Tommy Twotoes", :email => "tommyt2@example.com")
-  end  
 end
