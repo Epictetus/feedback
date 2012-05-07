3fb8e650994b076349bc5fe761fdbb4b51a8c1e3
Taurus Colvin <taurus.colvin@gmail.com>
2012-05-07 04:08:51 -0400

    account for IDs of arbitrary length

--- app/assets/javascripts/feedback/commentable.js
+++ app/assets/javascripts/feedback/commentable.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function($) {
   $('.reply-to-comment-btn').click(function(e) {
-    var commentID = /reply-to-comment-(\d)-btn/.exec(this.id)[1], 
+    var commentID = /reply-to-comment-(\d*)-btn/.exec(this.id)[1], 
         replyFormID = '#reply-to-comment-' + commentID + '-form', 
         replyButtonID = '#reply-to-comment-' + commentID + '-btn', 
         replyButtonTxt = $(replyButtonID).text(); 
