809fde5b549fcae9b3a41b6fa56925ecd1091f24
Taurus Colvin <taurus.colvin@gmail.com>
2012-05-08 21:14:53 -0400

    pull sort by select into helper and conditionalize inclusion of likes and rating criteria based on presence of other modules

--- app/helpers/feedback/comments_helper.rb
+++ app/helpers/feedback/comments_helper.rb
@@ -9,4 +9,11 @@ module Feedback::CommentsHelper
     polymorphic_url([commentable, Feedback::Comment], options)
   end
   
+  def sort_by_select_tag(options = {})
+    criteria = ["Oldest first", "Newest first"]
+    criteria.unshift("Rating") if defined?(Feedback::Models::Reviewable)
+    criteria.unshift("Popularity") if defined?(Feedback::Models::Likable)
+    select_tag "sort_by", options_for_select(criteria)
+  end
+  
 end
