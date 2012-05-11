62dc3c0701d02e28695f2332b2ffcfffbbbbacdb
Taurus Colvin <taurus.colvin@gmail.com>
2012-05-10 21:42:48 -0400

    dummy app ui refinements

--- spec/dummy/app/models/blog_post.rb
+++ spec/dummy/app/models/blog_post.rb
@@ -1,4 +1,5 @@
 class BlogPost < ActiveRecord::Base
+  attr_protected
   has_comments
-  attr_accessible :body, :title, :user_id
+  belongs_to :author, :class_name => "User", :foreign_key => "user_id"
 end
