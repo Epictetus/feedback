7584d5d011240bb346e3ed1013c67a4538991239
Taurus Colvin <taurus.colvin@gmail.com>
2012-05-10 23:02:03 -0400

    add support for comment admin table filtering

--- app/assets/javascripts/feedback/commentable.js
+++ app/assets/javascripts/feedback/commentable.js
@@ -1,40 +1,49 @@
 jQuery(document).ready(function($) {
-  
+
   //control for comment reply button
   $('.reply-to-comment-btn').click(function(e) {
     var commentID = /reply-to-comment-(\d*)-btn/.exec(this.id)[1], 
-        replyFormID = '#reply-to-comment-' + commentID + '-form', 
-        replyButtonID = '#reply-to-comment-' + commentID + '-btn', 
-        replyButtonTxt = $(replyButtonID).text(); 
-    
+    replyFormID = '#reply-to-comment-' + commentID + '-form', 
+    replyButtonID = '#reply-to-comment-' + commentID + '-btn', 
+    replyButtonTxt = $(replyButtonID).text(); 
+
     //toggle reply form
     $(replyFormID).slideToggle();
-    
+
     //toggle reply button text
     replyButtonTxt = replyButtonTxt === 'Reply' ? 'Cancel Reply' : 'Reply';
     $(replyButtonID).text(replyButtonTxt);
-    
+
     //prevent link from being followed
     e.preventDefault();
   });
-  
+
   //control for comment list sort by select
   $('#sort_comments_by').change(function(e) {
     $(this).parents('form').submit();
   });
-  
-  $('table#comment-list').dataTable( {
+
+  var oTable = $('table#comment-list').dataTable( {
     "sDom": "<'row'<'span6'l><'span6'f>r>t<'row'<'span6'i><'span6'p>>", 
     "sPaginationType": "bootstrap"
   });
-  
+
   $.extend( $.fn.dataTableExt.oStdClasses, {
     "sWrapper": "dataTables_wrapper form-inline"
   });
-  
+
   $.extend( $.fn.dataTableExt.oStdClasses, {
     "sSortAsc": "header headerSortDown",
     "sSortDesc": "header headerSortUp",
     "sSortable": "header"
   });
+
+  /* Add a select menu (for filtering) in each TH element in the table footer */
+  $("tfoot th").each( function ( i ) {
+    this.innerHTML = fnCreateSelect( oTable.fnGetColumnData(i) );
+    $('select', this).change( function () {
+      oTable.fnFilter( $(this).val(), i );
+    });
+  });
+  
 });
--- app/assets/javascripts/feedback/index.js
+++ app/assets/javascripts/feedback/index.js
@@ -1,3 +1,4 @@
 //= require ./commentable
 //= require ./datatables/jquery.dataTables.min
 //= require ./datatables/paging
+//= require ./datatables/filtering
